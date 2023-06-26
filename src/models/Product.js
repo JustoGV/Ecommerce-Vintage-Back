f8260da326b19fc147e1480275925634d604d9f9
Justo Gonzalez Viescas <jutitoogv@gmail.com>
2023-06-26 12:36:47 -0300

    model

--- src/models/Product.js
+++ src/models/Product.js
@@ -6,6 +6,7 @@ const Product = mongoose.Schema({
     description:{type: String, required: true},
     category:{type: String, required: true},
     price:{type: Number, required: true},
+    ventas:{type: Number, required: true},
     imagenURL: { type: String, required: true },
 })
 const productModel = mongoose.model('Product', Product)
