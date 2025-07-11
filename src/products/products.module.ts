e17540092e301a0d002b511b8352c5dd90de39ce
JustoGV <justitoogv@gmail.com>
2025-07-11 15:14:08 -0300

    module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,9 +4,6 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from '../entities/product.entity';
 import { Category } from '../entities/category.entity';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
-import * as fs from 'fs';
 
 @Module({
   imports: [
