51e49f3ce35ae6386c6025ed10868e8bdf8162ca
JustoGV <justitoogv@gmail.com>
2025-07-16 10:41:36 -0300

    changes

--- dist/main.js
+++ dist/main.js
@@ -6,12 +6,8 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: [
-            'http://localhost:3000',
-            'https://ecommerce-front-ruby.vercel.app',
-        ],
+        origin: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-        credentials: true,
     });
     app.useGlobalPipes(new common_1.ValidationPipe({
         transform: true,
--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://ecommerce-front-ruby.vercel.app',
-    ],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
   });
+
 
   app.useGlobalPipes(
     new ValidationPipe({
