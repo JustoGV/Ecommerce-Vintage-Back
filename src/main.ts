3282ec22d66eaf7fd548a70383150e3cdc98b93e
JustoGV <justitoogv@gmail.com>
2025-10-07 12:57:36 -0300

    funcio

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
+  // Agregar prefijo global para las APIs
+  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
